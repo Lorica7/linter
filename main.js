ec818d99762f9b590efc9b7fc2fa9402993edbd8
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-11-06 11:46:01 -0400

    added UI

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Global Variables
+
 let story =
   'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
@@ -7,7 +9,11 @@ let unnecessaryWords = ['extremely', 'literally', 'actually'];
 
 let storyWords = story.split (' ');
 
-console.log (storyWords.length);
+//Initial Word Cound
+
+const firstCount = storyWords.length;
+
+//Filter unneccesary words
 
 let betterWords = storyWords.filter (function (el) {
   if (!unnecessaryWords.includes (el)) {
@@ -17,7 +23,9 @@ let betterWords = storyWords.filter (function (el) {
 
 console.log (betterWords.length);
 
-const wordCounter = function (array1, array2) {
+// Count number of overused words
+
+const overusedCounter = function (array1, array2) {
   let counter = 0;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[0]) {
@@ -32,7 +40,9 @@ const wordCounter = function (array1, array2) {
   return counter;
 };
 
-console.log (wordCounter (storyWords, overusedWords));
+console.log (overusedCounter (storyWords, overusedWords));
+
+//Count sentences
 
 let sentCounter = 0;
 
@@ -44,20 +54,53 @@ storyWords.forEach (function (item) {
 
 console.log (sentCounter);
 
+//Creat e counter object, with three count props
+
 let counterObj = {
   wordCount: storyWords.length,
   sentCount: sentCounter,
-  overUsedWordCount: wordCounter (storyWords, overusedWords),
+  overUsedWordCount: overusedCounter (storyWords, overusedWords),
 };
 
 console.log (counterObj);
 
 function logCounters () {
   console.log (
-    `There are ${storyWords.length} words in this story. It has ${sentCounter} sentences. The number of overused words is ${wordCounter (storyWords, overusedWords)}.`
+    `There are ${storyWords.length} words in this story. It has ${sentCounter} sentences. The number of overused words is ${overusedCounter (storyWords, overusedWords)}.`
   );
 }
 
 logCounters ();
 
-console.log (betterWords.join (' '));
+let newStory = betterWords.join (' ');
+
+// Remove punctuation with Regex and then make all lower case
+
+newStory = newStory.replace (/[^\w\s]|_/g, '').replace (/\s+/g, ' ');
+const np = newStory.toLowerCase ().split (' ');
+
+// Count Individual words, find the most used
+
+let collection = [];
+
+np.forEach (function (el) {
+  let trackerObj = {
+    word: el,
+    num: 0,
+  };
+
+  for (i = 0; i < np.length; i++) {
+    if (np[i] == el) {
+      trackerObj.num = trackerObj.num + 1;
+    }
+  }
+  collection.push (trackerObj);
+});
+
+console.log (collection);
+
+const sortedTracker = collection.sort ((a, b) => a.num < b.num);
+
+let mostUsed = sortedTracker.shift ();
+
+console.log (mostUsed);
